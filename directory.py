a010b3c9bbf76443f6024d3b8cd2de8d16810e1a
Pooja <you@example.com>
2017-09-03 20:59:10 +0530

    assignment-contacts manager

---
+++ directory.py
@@ -0,0 +1,20 @@
+cont_list = {'pooja': [9999999999],'lakha':[8888888888]}
+
+# Number of contacts you want to add or update
+
+n = int(input('Enter contacts limit - '))
+
+for i in range(n):
+	name = input()
+	number = input()
+
+	if name in cont_list:
+		#update number if name exists
+		cont_list.setdefault(name,[]).append(number)
+	else:
+		#add name and number if name doesn't exixt
+		cont_list[name] = [name]
+		cont_list[name] = [number]
+
+print(cont_list)
+
