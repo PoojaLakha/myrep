553b47038194d417017f671220c82230c0cf1d49
Pooja <you@example.com>
2017-09-05 18:16:18 +0530

    Contact manager-enhanced

--- directory.py
+++ directory.py
@@ -1,22 +1,62 @@
 from collections import defaultdict
 
+def GetOption():
+	print()
+	print("1. Add/Update a contact")
+	print("2. Delete a contact")
+	print("3. Print contact details")
+	print("4. Print all contacts")
+	print("5. Exit")
+	return input("\nPlease enter your choice: ")
+
+def DoAddUpdate():
+	name = input("\nEnter Contact Name: ")
+	number = input("Enter Contact Number: ")
+
+	if name in cont_list:
+		cont_list[name].append(number)
+		print("Contact is updated")
+	else:
+		cont_list[name] = [name]
+		cont_list[name] = [number]
+		print("Contact is added")
+
+
+def DoDelete():
+	name = input("\nEnter Contact Name: ")
+
+	if name in cont_list:
+		del cont_list[name]
+		print("Contact is deleted")
+	else:
+		print("Contact does not exist to delete")
+
+def Display():
+	name = input("\nEnter Contact Name: ")
+
+	if name in cont_list:
+		print(name,cont_list[name])
+	else:
+		print("Contact not in list to display")
+
+def DisplayAll():
+	for contact in cont_list:
+		print(contact,cont_list[contact])	
+
 cont_list = defaultdict(list)
 cont_list = {'pooja': [9999999999],'lakha':[8888888888]}
 
-# Number of contacts you want to add or update
+Option = GetOption()
 
-n = int(input('Enter contacts limit - '))
-
-for i in range(n):
-	name = input()
-	number = input()
-
-	if name in cont_list:
-		#update number if name exists
-		cont_list[name].append(number)
-	else:
-		#add name and number if name doesn't exixt
-		cont_list[name] = [name]
-		cont_list[name] = [number]
-
-print(cont_list)
+while not Option == "5":
+	if Option == "1":
+		DoAddUpdate()
+	elif Option == "2":
+		DoDelete()
+	elif Option == "3":
+		Display()
+	elif Option == "4":
+		DisplayAll()
+	elif Option not in['1','2','3','4','5']:
+		print("Enter options only between 1 to 5")
+	Option = GetOption()
