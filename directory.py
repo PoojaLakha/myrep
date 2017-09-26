e672f9adccbdaef75b9e14068612019dab3b7ff1
Pooja <you@example.com>
2017-09-26 11:40:26 +0530

    using __main__ function

--- directory.py
+++ directory.py
@@ -1,90 +1,25 @@
-from peewee import *
+from di_op import (initialize_db, do_add_update, get_option, do_delete,
+                   display, display_all, db)
 
-def initialize_db():
-	db.connect()
-	try:
-		db.create_table(contact)
-	except OperationalError:
-		print("Contact table already exists!")
+# from peewee import CharField, ForeignKeyField, SqliteDatabase, Model
 
-def GetOption():
-	print()
-	print("1. Add/Update a contact")
-	print("2. Delete a contact")
-	print("3. Print contact details")
-	print("4. Print all contacts")
-	print("5. Exit")
-	return input("\nPlease enter your choice: ")
+if __name__ == '__main__':
 
-def DoAddUpdate():
+    initialize_db()
 
-	Name = input("\nEnter Contact Name: ")
-	Number = input("Enter Contact Number: ")
+    option = get_option()
 
-	cont = contact.select().where(contact.name == Name and contact.number == Number).first()
+    while not option == "5":
+        if option == "1":
+            do_add_update()
+        elif option == "2":
+            do_delete()
+        elif option == "3":
+            display()
+        elif option == "4":
+            display_all()
+        elif option not in['1', '2', '3', '4', '5']:
+            print("Enter options only between 1 to 5")
+        option = get_option()
 
-	if not cont:
-		cont = contact(name=Name, number=int(Number))
-		cont.save()
-		print("Contact added successfully")
-	else:
-		print("Name and number already exist")
-
-def DoDelete():
-	Name = input("\nEnter Contact Name: ")
-	Number = input("Enter Contact Number: ")
-
-	del_cont = contact.select().where(contact.name==Name and contact.number==Number).first()
-
-	if not del_cont:
-		print("Name and(or) number does not exist, check again(using option 3 or 4)")
-	else:
-		contact.get(contact.name==Name and contact.number==Number)
-		del_cont.delete_instance()
-		print("Contact deleted successfully")
-
-def Display():
-
-	Name = input("\nEnter Contact Name: ")
-
-	print(Name)
-
-	for cont in contact.select().where(contact.name == Name):
-		print(cont.number)
-
-def DisplayAll():
-	
-	for cont in contact.select(contact.name).distinct():
-		print(cont.name+":")
-		for num in contact.select().where(contact.name == cont.name):
-			print(num.number)
-		print("")
-
-db = SqliteDatabase('contacts.db')
-
-class contact(Model):
-    name = CharField()
-    number = IntegerField()
-    class Meta:
-    	database = db 
-
-initialize_db()
-#cont = contact.create(name = "pooja", number = 9696969696)
-#cont.save()
-
-Option = GetOption()
-
-while not Option == "5":
-	if Option == "1":
-		DoAddUpdate()
-	elif Option == "2":
-		DoDelete()
-	elif Option == "3":
-		Display()
-	elif Option == "4":
-		DisplayAll()
-	elif Option not in['1','2','3','4','5']:
-		print("Enter options only between 1 to 5")
-	Option = GetOption()
- 
-db.close()
+    db.close()
