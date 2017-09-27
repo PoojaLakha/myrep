5a50d0efcce99a51861c7686d928b9a295d4e6f1
Pooja <you@example.com>
2017-09-28 01:01:45 +0530

    enhancements made on functions

--- db_operations.py
+++ db_operations.py
@@ -1,101 +1,89 @@
+from collections import defaultdict
+
 from peewee import (CharField, ForeignKeyField, SqliteDatabase, Model,
                     OperationalError)
 
 
-db = SqliteDatabase('contacts.db')
+DB = SqliteDatabase('contacts.db')
 
 
-class contact_name(Model):
+class ContactName(Model):
     name = CharField(primary_key=True)
 
     class Meta:
-        database = db
+        database = DB
 
 
-class contact_number(Model):
+class ContactNumber(Model):
     number = CharField()
-    name = ForeignKeyField(contact_name)
+    name = ForeignKeyField(ContactName)
 
     class Meta:
-        database = db
+        database = DB
 
 
 def initialize_db():
-    db.connect()
+    DB.connect()
     try:
-        db.create_table(contact_name)
+        DB.create_table(ContactName)
     except OperationalError:
-        print("Contact_name table already exists!")
+        pass
 
     try:
-        db.create_table(contact_number)
+        DB.create_table(ContactNumber)
     except OperationalError:
-        print("Contact_number table already exists!")
-
-    # contact = contact_name.create(name="pooja")
-    # contact.save()
-    # contact = contact_number.create(name="pooja", number="9999999999")
-    # contact.save()
+        pass
 
 
-def get_option():
-    print()
-    print("1. Add/Update a contact")
-    print("2. Delete a contact")
-    print("3. Print contact details")
-    print("4. Print all contacts")
-    print("5. Exit")
-    return input("\nPlease enter your choice: ")
+def close_db():
+    DB.close()
 
 
-def do_add_update():
+def do_add_update(iname, inumber):
 
-    iname = input("\nEnter Contact Name: ")
-    inumber = input("Enter Contact Number: ")
-
-    contact_name.get_or_create(name=iname)
-
-    contact_number.get_or_create(name=iname, number=inumber)
-
-    print("Contact details added/updated successfully")
+    ContactName.get_or_create(name=iname)
+    ContactNumber.get_or_create(name=iname, number=inumber)
 
 
-def do_delete():
+def do_delete(iname):
 
-    iname = input("\nEnter Contact Name: ")
+    contact_found = ContactName.select().where(
+        ContactName.name == iname).first()
 
-    deletecontact = contact_name.select().where(
-        contact_name.name == iname).first()
-
-    if not deletecontact:
-        print("Contact doesn't exist")
+    if not contact_found:
+        return False
     else:
-        for contactdelete in contact_number.select().where(
-                contact_number.name == iname):
-            contactdelete.delete_instance()
-        contact_name.get(contact_name.name == iname)
-        deletecontact.delete_instance()
-        print("Contact deleted successfully")
+        delete_contact = ContactName.select().where(
+            ContactName.name == iname)
+        ContactNumber.delete().where(ContactNumber.name.in_(
+            delete_contact)).execute()
+
+        ContactName.get(ContactName.name == iname)
+        contact_found.delete_instance()
+        return True
 
 
-def display():
-    Contact_present = False
-    iname = input("\nEnter Contact Name: ")
+def display(iname):
 
-    for displaycontact in contact_number.select().where(
-            contact_number.name == iname):
-        Contact_present = True
-        print(displaycontact.number)
-    else:
-        if not Contact_present:
-            print("No contacts to display")
+    contact_list = []
+    contact_present = False
+
+    for display_contact in ContactNumber.select().where(
+            ContactNumber.name == iname):
+        if display_contact:
+            contact_present = True
+            contact_list.append(display_contact.number)
+
+    return contact_list, contact_present
 
 
 def display_all():
 
-    for displayname in contact_name.select():
-        print(displayname.name + ":")
-        for displaynumber in contact_number.select().where(
-                contact_number.name == displayname.name):
-            print(displaynumber.number)
-        print("")
+    all_contacts = defaultdict(list)
+
+    for display_name in ContactName.select():
+        for display_number in ContactNumber.select().where(
+                ContactNumber.name == display_name.name):
+            all_contacts[display_name.name].append(display_number.number)
+
+    return all_contacts
